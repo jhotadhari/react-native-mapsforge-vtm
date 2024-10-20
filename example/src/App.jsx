a77b13a20b110f230ec0c6c15aaf23e3bf595209
jhotadhari <tellme@waterproof-webdesign.de>
2024-10-20 17:27:52 +0200

    Clean up example App

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -18,9 +18,6 @@ import {
 	View,
 	NativeEventEmitter,
 } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
-console.log( 'debug GestureHandlerRootView', GestureHandlerRootView ); // debug
 
 /**
  * react-native-mapsforge dependencies
@@ -47,7 +44,6 @@ const { MapContainerModule } = nativeMapModules;
 /**
  * Internal dependencies
  */
-import ChartWrapper from './components/ChartWrapper.jsx';
 import PickerModalControl from './components/PickerModalControl.jsx';
 import usePermissionsOk from './compose/usePermissionsOk.jsx';
 import { randomNumber } from './utils';
@@ -120,50 +116,21 @@ const App = () => {
 	const { permissionsOk, requestPermission } = usePermissionsOk();
 
 
-	const [barTopHeight,setBarTopHeight] = useState( 0 );
-	const [barBottomHeight,setBottomTopHeight] = useState( 0 );
+	const [barTopHeight,setBarTopHeight] = useState( null );
+	const [barBottomHeight,setBottomTopHeight] = useState( null );
 
 
 
 
-	// const [randomCenter,setRandomCenter] = useState( [-12.65, -75.239] );
-	// const doNewRandomCenter = () => setRandomCenter( [
-	// 	randomNumber( -12, -13 ),	// lat
-	// 	randomNumber( -74, -75 ),	// long
-	// ] );
-
-	// const [randomZoom,setRandomZoom] = useState( 8 );
-	// const doNewRandomZoom = () => setRandomZoom( Math.round( randomNumber( 8, 16 ) ) );
-
-	// const [randomMinZoom,setRandomMinZoom] = useState( 2 );
-	// const doNewRandomMinZoom = () => setRandomMinZoom( Math.round( randomNumber( 5, 20 ) ) );
-
-	// const [randomMaxZoom,setRandomMaxZoom] = useState( 20 );
-	// const doNewRandomMaxZoom = () => setRandomMaxZoom( Math.round( randomNumber( 5, 20 ) ) );
-
-
-	// const [randomViewportVal,setRandomViewportVal] = useState( 0 );
-	// const doNewViewportVal = () => setRandomViewportVal( Math.round( randomNumber( 200, 400 ) ) );
-
-
-
-
-	// const [moveEnabled,setMoveEnabled] = useState( true );
-	// const [tiltEnabled,setTiltEnabled] = useState( true );
-	// const [rotationEnabled,setRotationEnabled] = useState( true );
-	// const [zoomEnabled,setZoomEnabled] = useState( true );
-
+	const [randomCenter,setRandomCenter] = useState( [-12.65, -75.239] );
+	const doNewRandomCenter = () => setRandomCenter( [
+		randomNumber( -12, -13 ),	// lat
+		randomNumber( -74, -75 ),	// long
+	] );
 
 	const promiseQueueState = usePromiseQueueState();
 
-	// const [mapFile, setMapFile] = useState( mapFileOptions[0].value );
-	// const [showLayerMapsforge, setShowLayerMapsforge] = useState( true );
-	const [showLayerBitmapTile, setShowLayerBitmapTile] = useState( true );
-	// const [showMarkers, setShowMarkers] = useState( true );
-
 	const [mainMapViewId, setMainMapViewId] = useState( null );
-
-	// const [iconIndex, setIconIndex] = useState( 0 );
 
 	const [renderOverlayOptions, setRenderOverlayOptions] = useState( [] );
 
@@ -191,31 +158,6 @@ const App = () => {
 	] );
 	const [renderTheme, setRenderTheme] = useState( renderThemeOptions.find( o => o.label === 'Alti' ).value );
 
-
-	const [coordinates, setCoordinates] = useState( [] );
-	const [coordinatesSimplified, setCoordinatesSimplified] = useState( [] );
-
-
-
-	const [slopeSimplificationTolerance, setSlopeSimplificationTolerance] = useState( 7 );
-	const [flattenWindowSize, setFlattenWindowSize] = useState( 9 );
-	const [strokeWidth, setStrokeWidth] = useState( 4 );
-	const [slopeColors, setSlopeColors] = useState( [
-		[-25, '#000a70'],
-		[-10, '#0000ff'],
-		[-5, '#01c2ff'],
-		[0, '#35fd2d'],
-		[5, '#f9ff00'],
-		[10, '#ff0000'],
-		[25, '#810500'],
-	] );
-	const swapSlopeColors = () => {
-		const newSlopeColors = [...slopeColors].reverse().map( ( arr, index ) => ( [slopeColors[index][0],arr[1]] ) );
-		setSlopeColors( newSlopeColors );
-	};
-
-
-
 	useEffect( () => {
 		const eventEmitter = new NativeEventEmitter();
 		let eventListener = eventEmitter.addListener( 'onHardwareKeyUp', result => {
@@ -236,25 +178,6 @@ const App = () => {
 		};
 	}, [mainMapViewId] );
 
-	// useEffect( () => {
-	// 	const eventEmitter = new NativeEventEmitter();
-	// 	let eventListener = eventEmitter.addListener( 'RenderThemeParsed', result => {
-
-
-	// 		Object.keys( result?.collection ).map( key => {
-
-	// 			console.log( 'debug RenderThemeParsed', key, result.collection[key] ); // debug
-
-
-	// 		} );
-
-	// 	} );
-	// 	return () => {
-	// 		eventListener.remove();
-	// 	};
-	// }, [] );
-
-
 	const {
 		renderStyleDefaultId,
 		renderStyleOptions,
@@ -266,24 +189,11 @@ const App = () => {
 	const [renderStyle, setRenderStyle] = useState( renderStyleDefaultId );
 
 
-	// const test = {
-	// 	renderOverlayOptions,
-	// 	renderOverlays,
-	// 	renderOverlayOptionsCount: renderOverlayOptions.length,
-	// 	renderOverlaysCount: renderOverlays.length,
-	// 	// renderTheme,
-	// 	// renderStyle,
-	// 	// renderStyleDefaultId,
-	// 	// renderStyleOptions,
-	// };
-	// console.log( 'debug test', test ); // debug
-
-
-
 	useEffect( () => {
 		if ( ! renderStyle && renderStyleDefaultId ) {
 			setRenderStyle( renderStyleDefaultId );
 		}
+
 		if ( ! renderOverlayOptions.length ) {
 			const renderStyleOptions_ = renderStyleOptions.find( opt => opt.value === renderStyle );
 			if ( undefined !== renderStyleOptions_ ) {
@@ -293,6 +203,8 @@ const App = () => {
 						label: renderStyleOptions_.options[value],
 					};
 				} );
+
+				// console.log( 'debug test newItems', newItems ); // debug
 				setRenderOverlayOptions( newItems );
 			}
 		}
@@ -308,15 +220,11 @@ const App = () => {
 		height,
 	} = useWindowDimensions();
 
-	const mapHeight = height - barTopHeight - barBottomHeight;
-
-
-	// ??? TODO hillshading layer.
-	// 		see vtm MBTilesBitmapTileDataSource, MBTilesTileDataSource, ITileDataSource
-	// 		see mapsforge MemoryCachingHgtReaderTileSource.getHillshadingBitmap
+	const mapHeight = barTopHeight && barBottomHeight
+		? height - barTopHeight - barBottomHeight
+		: null;
 
 	return (
-		// <GestureHandlerRootView style={{ flex: 1 }}>
 		<SafeAreaView style={ {
 			...style,
 			height,
@@ -371,112 +279,118 @@ const App = () => {
 						style={ {
 							flexDirection: 'row',
 							width,
-							justifyContent: 'space-around',
+							justifyContent: 'space-evenly',
 							alignItems: 'center',
 							marginBottom: 10,
 						} }
 					>
-
-
-						<Text style={ {width: 100} }>Simplification</Text>
-
-						<Text style={ {width: 30} }>{ slopeSimplificationTolerance }</Text>
-
 						<Button
-							onPress={ () => setSlopeSimplificationTolerance( slopeSimplificationTolerance + 1 ) }
-							title="  +  "
+							onPress={ () => doNewRandomCenter() }
+							title="rand center"
 							disabled={ promiseQueueState > 0 }
 						/>
-						<Button
-							onPress={ () => setSlopeSimplificationTolerance( Math.max( 0, slopeSimplificationTolerance -1 ) ) }
-							title="  -  "
-							disabled={ promiseQueueState > 0 || slopeSimplificationTolerance == 0 }
-						/>
-
-
 					</View>
+				</View>
 
 
-					<View
-						style={ {
-							flexDirection: 'row',
-							width,
-							justifyContent: 'space-around',
-							alignItems: 'center',
-							marginBottom: 10,
+				{ mapHeight && <MapContainer
+					height={ mapHeight }
+					center={ randomCenter }
+					zoomLevel={ 12 }
+					mapViewNativeTag={ mainMapViewId }
+					setMapViewNativeTag={ setMainMapViewId }
+					minZoom={ 2 }
+					maxZoom={ 20 }
+					moveEnabled = { true }
+					tiltEnabled = { false }
+					rotationEnabled = { false }
+					zoomEnabled = { true }
+					onPause={ result => {
+						console.log( 'debug lifecycle event onPause', result );
+					} }
+					onResume={ result => {
+						console.log( 'debug lifecycle event onResume', result );
+					} }
+				>
+
+					{/* <MapEvents
+						nativeTag={ mainMapViewId }
+					/> */}
+
+
+					{/* { showLayerBitmapTile && <LayerBitmapTile
+						url={ 'https://mt1.google.com/vt/lyrs=r&x={X}&y={Y}&z={Z}' }
+						cacheSize={ 10 * 1024 * 1024 }
+					/> } */}
+
+					<LayerMBTilesBitmap
+						mapFile={ '/storage/emulated/0/Documents/orux/mapfiles/OAM-World-1-10-J70.mbtiles' }
+					/>
+
+					<LayerMapsforge
+						mapFile={ '/storage/emulated/0/Documents/orux/mapfiles/Peru-Ecuador_oam.osm.map' }
+						renderTheme={ renderTheme }
+						renderStyle={ renderStyle }
+						renderOverlays={ renderOverlays }
+					/>
+
+					<LayerHillshading
+						hgtDirPath="/storage/emulated/0/Documents/orux/dem"
+						cacheSize={ 512 }
+						zoomMin={ 2 }
+						zoomMax={ 20 }
+						shadingAlgorithm={ LayerHillshading.shadingAlgorithms.SIMPLE }
+						magnitude={ 90 }
+						shadingAlgorithmOptions={ {
+							linearity: -1,
+							scale: 1,
 						} }
-					>
-
-						<Text style={ {width: 100} }>Flattening</Text>
-						<Text style={ {width: 30} }>{ flattenWindowSize }</Text>
+					/>
 
 
+					<LayerPathSlopeGradient
+						strokeWidth={ 5 }
+						slopeColors={ [
+							[-25, '#000a70'],
+							[-10, '#0000ff'],
+							[-5, '#01c2ff'],
+							[0, '#35fd2d'],
+							[5, '#f9ff00'],
+							[10, '#ff0000'],
+							[25, '#810500'],
+						] }
+						filePath={ '/storage/emulated/0/Documents/orux/tracklogs/2024-10-09 0817__20241009_0817.gpx' }
+					/>
 
-						<Button
-							onPress={ () => setFlattenWindowSize( flattenWindowSize + 2 ) }
-							title="  +  "
-							disabled={ promiseQueueState > 0 }
-						/>
-						<Button
-							onPress={ () => setFlattenWindowSize( Math.max( 5, flattenWindowSize - 2 ) ) }
-							title="  -  "
-							disabled={ promiseQueueState > 0 || flattenWindowSize == 5 }
-						/>
 
-					</View>
-
-					<View
-						style={ {
-							flexDirection: 'row',
-							width,
-							justifyContent: 'space-around',
-							alignItems: 'center',
-							marginBottom: 10,
+					{/* }
+					{ showMarkers && [...locations].map( ( latLong, index ) => <Marker
+						latLong={ latLong }
+						key={ index }
+						tabDistanceThreshold={ 80 }
+						icon={ icons[iconIndex] }
+						onTab={ res => {
+							console.log( 'debug Marker res', res ); // debug
 						} }
-					>
+					/> ) } */}
 
-						<Text style={ {width: 100} }>strokeWidth</Text>
-						<Text style={ {width: 30} }>{ strokeWidth }</Text>
+					<LayerScalebar/>
 
-
-						<Button
-							onPress={ () => setStrokeWidth( strokeWidth + 1 ) }
-							title="  +  "
-							disabled={ promiseQueueState > 0 }
-						/>
-						<Button
-							onPress={ () => setStrokeWidth( Math.max( 0, strokeWidth - 1 ) ) }
-							title="  -  "
-							disabled={ promiseQueueState > 0 || strokeWidth == 0 }
-						/>
-
-					</View>
+				</MapContainer> }
 
 
-					<View
-						style={ {
-							flexDirection: 'row',
-							width,
-							justifyContent: 'space-around',
-							alignItems: 'center',
-							marginBottom: 10,
-						} }
-					>
-
-						<Text style={ {width: 100} }>slopeColors</Text>
-						<Text style={ {width: 30} }></Text>
-
-
-						<Button
-							onPress={ () => swapSlopeColors() }
-							title="swap"
-							disabled={ promiseQueueState > 0 }
-						/>
-
-					</View>
-
-
-					{/*
+				<View
+					onLayout={ e => {
+						const { height } = e.nativeEvent.layout;
+						setBottomTopHeight( height );
+					} }
+					style={ {
+						...style,
+						position: 'relative',
+						width,
+						paddingTop: 10,
+					} }
+				>
 
 					<View
 						style={ {
@@ -535,227 +449,13 @@ const App = () => {
 							} }
 							closeOnChange={ false }
 						/>
-					</View> */}
-
-				</View>
-
-
-				{ mapHeight && <MapContainer
-					// width={ width }
-					height={ mapHeight }
-					center={ [-12.65, -75.239] }	// ??? maybe other way around, should do everywhere same order!!!
-					zoomLevel={ 12 }
-					mapViewNativeTag={ mainMapViewId }
-					setMapViewNativeTag={ setMainMapViewId }
-					minZoom={ 2 }
-					maxZoom={ 20 }
-
-
-					tiltEnabled = { false }
-					rotationEnabled = { false }
-
-
-
-					onPause={ result => {
-						console.log( 'debug lifecycle event onPause', result );
-					} }
-					onResume={ result => {
-						console.log( 'debug lifecycle event onResume', result );
-					} }
-				>
-
-					{/* <MapEvents
-						nativeTag={ mainMapViewId }
-					/> */}
-
-
-					{/* { showLayerBitmapTile && <LayerBitmapTile
-						url={ 'https://mt1.google.com/vt/lyrs=r&x={X}&y={Y}&z={Z}' }
-						cacheSize={ 10 * 1024 * 1024 }
-					/> } */}
-
-
-					{/* { showLayerBitmapTile && <LayerMBTilesBitmap
-						mapFile={ '/storage/emulated/0/Documents/orux/mapfiles/OAM-World-1-10-J70.mbtiles' }
-					/> } */}
-
-					<LayerMapsforge
-						mapFile={ '/storage/emulated/0/Documents/orux/mapfiles/Peru-Ecuador_oam.osm.map' }
-						renderTheme={ renderTheme }
-						renderStyle={ renderStyle }
-						renderOverlays={ renderOverlays }
-					/>
-
-					{/* <LayerHillshading
-						hgtDirPath="/storage/emulated/0/Documents/orux/dem"
-						cacheSize={ 512 }
-						zoomMin={ 2 }
-						zoomMax={ 20 }
-						shadingAlgorithm={ LayerHillshading.shadingAlgorithms.SIMPLE }
-						magnitude={ 90 }
-						shadingAlgorithmOptions={ {
-							linearity: -1,
-							scale: 1,
-						} }
-					/> */}
-
-					<LayerPathSlopeGradient
-						slopeSimplificationTolerance={ slopeSimplificationTolerance }
-						flattenWindowSize={ flattenWindowSize }
-						strokeWidth={ strokeWidth }
-						slopeColors={ slopeColors }
-						onCreate={ response => {
-							if ( response.coordinates ) {
-								setCoordinates( response.coordinates );
-							}
-							if ( response.coordinatesSimplified ) {
-								setCoordinatesSimplified( response.coordinatesSimplified );
-							}
-						} }
-						onChange={ response => {
-
-							console.log( 'debug response', response ); // debug
-							if ( response.coordinates ) {
-								setCoordinates( response.coordinates );
-							}
-							if ( response.coordinatesSimplified ) {
-								setCoordinatesSimplified( response.coordinatesSimplified );
-							}
-						} }
-						onRemove={ () => {
-							setCoordinates( [] );
-							setCoordinatesSimplified( [] );
-						} }
-						responseInclude={ {
-							coordinates: 1,
-							coordinatesSimplified: 2,
-						} }
-						filePath={ '/storage/emulated/0/Android/media/jhotadhari.reactnative.mapsforge.vtm.example/dummy/randomTrack.gpx' }
-					/>
-
-
-					{/* <LayerPathSlopeGradient
-						filePath={ '/storage/emulated/0/Documents/orux/tracklogs/2024-10-08 0900__20241008_0900.gpx' }
-					/>
-
-					<LayerPathSlopeGradient
-						filePath={ '/storage/emulated/0/Documents/orux/tracklogs/2024-10-07 1000__20241007_1000.gpx' }
-					/>
-
-					<LayerPathSlopeGradient
-						filePath={ '/storage/emulated/0/Documents/orux/tracklogs/2024-10-06 1026__20241006_1026.gpx' }
-					/> */}
-
-
-
-					{/* }
-					{ showMarkers && [...locations].map( ( latLong, index ) => <Marker
-						latLong={ latLong }
-						key={ index }
-						tabDistanceThreshold={ 80 }
-						icon={ icons[iconIndex] }
-						onTab={ res => {
-							console.log( 'debug Marker res', res ); // debug
-						} }
-					/> ) } */}
-
-					{/* <LayerScalebar/> */}
-
-				</MapContainer> }
-
-
-				<View
-					onLayout={ e => {
-						const { height } = e.nativeEvent.layout;
-						setBottomTopHeight( height );
-					} }
-					style={ {
-						...style,
-						position: 'relative',
-						width,
-						// paddingTop: 10,
-					} }
-				>
-
-					<ChartWrapper
-						coordinates={ coordinates }
-						coordinatesSimplified={ coordinatesSimplified }
-					/>
-
+					</View>
 				</View>
 
 			</View> }
 
 
-
-				{/* <View
-					style={ {
-						...style,
-						flexDirection: 'row',
-						justifyContent: 'space-evenly',
-						alignItems: 'center',
-						width,
-						marginBottom: 10,
-					} }
-				>
-
-					<Text>{ promiseQueueState > 0 ? 'busy' : 'idle'  }</Text>
-
-					<Button
-						onPress={ () => {
-							promiseQueue.enqueue( () => {
-								MapContainerModule.zoomIn( mainMapViewId );
-							} );
-						} }
-						title="+"
-						disabled={ promiseQueueState > 0 }
-					/>
-					<Button
-						onPress={ () => {
-							promiseQueue.enqueue( () => {
-								MapContainerModule.zoomOut( mainMapViewId );
-							} );
-						} }
-						title="-"
-						disabled={ promiseQueueState > 0 }
-					/>
-				</View>
-
-				<View
-					style={ {
-						...style,
-						flexDirection: 'row',
-						justifyContent: 'space-evenly',
-						alignItems: 'center',
-						width,
-						marginBottom: 10,
-					} }
-				>
-
-					<PickerModalControl
-						headerLabel={ 'Map file' }
-						options={ mapFileOptions }
-						values={ [mapFile] }
-						onChange={ clickedVal => setMapFile( clickedVal ) }
-						closeOnChange={ false }
-						disabled={ promiseQueueState > 0 }
-					/>
-
-					<PickerModalControl
-						headerLabel={ 'Render theme' }
-						options={ renderThemeOptions }
-						values={ [renderTheme] }
-						onChange={ clickedVal => setRenderTheme( clickedVal ) }
-						closeOnChange={ false }
-						disabled={ promiseQueueState > 0 }
-					/>
-
-				</View> */}
-
-			{/* </View> */}
 		</SafeAreaView>
-
-		// </GestureHandlerRootView>
 	);
 };
 
