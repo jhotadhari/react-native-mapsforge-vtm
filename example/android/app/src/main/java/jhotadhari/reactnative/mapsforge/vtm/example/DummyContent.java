3486f8c765ba84cbc045a139afa88a9da46780ca
jhotadhari <tellme@waterproof-webdesign.de>
2024-11-09 11:44:09 +0100

    Change DummyContent, implement updates from https://github.com/jhotadhari/straymap/commit/f3f974f0dc788131fb1c09e61b5666f85777ca74

--- example/android/app/src/main/java/jhotadhari/reactnative/mapsforge/vtm/example/DummyContent.java
+++ example/android/app/src/main/java/jhotadhari/reactnative/mapsforge/vtm/example/DummyContent.java
@@ -14,8 +14,9 @@ import java.util.Objects;
 public class DummyContent {
 
 	public static String getMediaDirPath( MainApplication app ) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-			return Arrays.stream(app.getExternalMediaDirs()).findFirst().get().toString();
+		File[] mediaDirs = app.getExternalMediaDirs();
+		if ( mediaDirs.length > 0 ) {
+			return mediaDirs[0].toString();
 		}
 		return null;
 	}
@@ -25,21 +26,26 @@ public class DummyContent {
 			AssetManager assetManager = app.getAssets();
 			String[] files = assetManager.list( parent );
 			for ( int i = 0; i < Objects.requireNonNull( files ).length; i++ ) {
-				if ( Objects.requireNonNull( assetManager.list(parent + "/" + files[i] ) ).length > 0 ) {
-					copyAssets( app, parent + "/" + files[i] );
+				File file = new File( files[i] );
+				if ( Objects.requireNonNull( assetManager.list(parent + File.separator + files[i] ) ).length > 0 ) {
+					File dir = getOutFile( getMediaDirPath( app ), parent, file );
+					if ( null != dir && ! dir.exists() ) {
+						dir.mkdirs();
+					}
+					copyAssets( app, parent + File.separator + files[i] );
 				} else {
-					File file = new File( files[i] );
-					String[] parentArr = parent.split( "/" );
-					parentArr = Arrays.copyOfRange( parentArr, 1, parentArr.length );
-					File outFile = new File(getMediaDirPath( app ) + "/" + String.join( "/", parentArr ) + file.getAbsolutePath() );
-					if ( ! outFile.exists() ) {
-						InputStream in = assetManager.open( parent + file.getAbsolutePath() );;
-						OutputStream out = new FileOutputStream( outFile );;
-						outFile.getParentFile().mkdirs();
-						copyFile( in, out );
-						in.close();
-						out.flush();
-						out.close();
+					String mediaDirPath = getMediaDirPath( app );
+					if ( null != mediaDirPath ) {
+						File outFile = getOutFile( mediaDirPath, parent, file );
+						if ( ! outFile.exists() ) {
+							InputStream in = assetManager.open( parent + file.getAbsolutePath() );;
+							FileOutputStream out = new FileOutputStream( outFile );;
+							outFile.getParentFile().mkdirs();
+							copyFile( in, out );
+							in.close();
+							out.flush();
+							out.close();
+						}
 					}
 				}
 			}
@@ -48,7 +54,16 @@ public class DummyContent {
 		}
 	}
 
-	private static void copyFile( InputStream in, OutputStream out ) throws IOException {
+	private static File getOutFile( String mediaDirPath, String parent, File file ) {
+		if ( null == mediaDirPath ) {
+			return null;
+		}
+		String[] parentArr = parent.split( File.separator );
+		parentArr = Arrays.copyOfRange( parentArr, 1, parentArr.length );
+		return new File(mediaDirPath + File.separator + String.join( File.separator, parentArr ) + file.getAbsolutePath() );
+	}
+
+	private static void copyFile( InputStream in, FileOutputStream out ) throws IOException {
 		byte[] buffer = new byte[1024];
 		int read;
 		while( ( read = in.read( buffer ) ) != -1 ){
@@ -57,9 +72,13 @@ public class DummyContent {
 	}
 
 	public static void init(MainApplication app ) {
-		File mediaDir = new File( getMediaDirPath( app ) );
-		if ( ! mediaDir.exists() ) {
-			mediaDir.mkdirs();
+		String mediaDirPath = getMediaDirPath( app );
+		if ( null != mediaDirPath ) {
+			File mediaDir = new File( mediaDirPath );
+			if ( ! mediaDir.exists() ) {
+				mediaDir.mkdirs();
+			}
+			copyAssets( app, "dummy" );
 		}
 		copyAssets( app, "dummy" );
 	}
