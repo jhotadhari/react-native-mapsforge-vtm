e6beade79541e6f6054c219641e8e761b2451b23
jhotadhari <tellme@waterproof-webdesign.de>
2026-07-02 23:31:35 +0200

    chore: add explicit buildCommand for npm publish

--- release.config.ts
+++ release.config.ts
@@ -1,4 +1,4 @@
 export default {
 	repo: 'https://github.com/jhotadhari/react-native-mapsforge-vtm',
-	publish: { npm: true, github: true },
+	publish: { npm: { buildCommand: 'yarn prepare' }, github: true },
 };
